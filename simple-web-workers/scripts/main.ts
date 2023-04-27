4cda11e24a84194fdcb2bda914bb3cd1168b3fa0
Eshan Chatterjee <eshan2001221@gmail.com>
2023-04-27 22:34:45 +0530

    simple-worker

--- simple-web-workers/scripts/main.ts
+++ simple-web-workers/scripts/main.ts
@@ -1,25 +1,34 @@
-const firstNumber = <HTMLInputElement>document.querySelector("#number1");
-const secondNumber = <HTMLInputElement>document.querySelector("#number2");
+const firstNumber = <HTMLInputElement>(
+	document.querySelector("#number1[type='number']")
+);
+const secondNumber = <HTMLInputElement>(
+	document.querySelector("#number2[type='number']")
+);
 
 const result = document.querySelector(".result");
 
-if(window.Worker && firstNumber && secondNumber && result) {
-    let myWorker = new Worker("worker.js");
+if (window.Worker && firstNumber && secondNumber && result) {
+	let myWorker = new Worker("worker.js");
 
-    firstNumber.onchange = function() {
-        myWorker.postMessage([firstNumber.value, secondNumber.value]);
-        console.log("Message posted to worker");
-    }
+	firstNumber.onchange = function () {
+		myWorker.postMessage([firstNumber.value, secondNumber.value]);
+		console.log("Message posted to worker");
+	};
 
-    secondNumber.onchange = function() {
-        myWorker.postMessage([firstNumber.value, secondNumber.value]);
-        console.log("Message posted to worker");
-    }
+	secondNumber.onchange = function () {
+		myWorker.postMessage([firstNumber.value, secondNumber.value]);
+		console.log("Message posted to worker");
+	};
 
-    myWorker.onmessage = function(e: MessageEvent<any>) {
-        result.textContent = e.data;
-        console.log("Message received from worker");
-    }
+	myWorker.onmessage = function (e: MessageEvent<any>) {
+		result.textContent = e.data;
+		console.log("Message received from worker");
+	};
+
+	myWorker.onerror = function (error) {
+		console.error(`Worker error: ${error.message} \n`);
+		throw error;
+	};
 } else {
-    console.error("Web Workers Not Supported!!");
+	console.error("Web Workers Not Supported!!");
 }
--- simple-web-workers/scripts/worker.ts
+++ simple-web-workers/scripts/worker.ts
@@ -1,13 +1,13 @@
-onmessage = function(e: MessageEvent<any>) {
-    console.log("Worker: Message received from Main script");
+onmessage = function (e: MessageEvent<any>) {
+	console.log("Worker: Message received from Main script");
 
-    const result: number = e.data[0] * e.data[1];  // Multiply the two numbers received from the main script
+	const result: number = e.data[0] * e.data[1]; // Multiply the two numbers received from the main script
 
-    if(isNaN(result)) {
-        postMessage("Please write two numbers");
-    } else {
-        const workerResult = "Result: " + result;
-        console.log("Worker: Posting message back to Main script");
-        postMessage(workerResult);
-    }
-}
+	if (isNaN(result)) {
+		postMessage("Please write two numbers");
+	} else {
+		const workerResult = "Result: " + result;
+		console.log("Worker: Posting message back to Main script");
+		postMessage(workerResult);
+	}
+};
